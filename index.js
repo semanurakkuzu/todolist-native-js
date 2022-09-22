b911e0ce03dc6e7d8b11933c601194c430f61d67
semanurakkuzu <semanurakkuzu@gmail.com>
2022-09-22 11:46:13 +0300

    git init

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 import './style.scss'
+
+const addFunction = (event) => {
+    event.preventDefault()
+
+    let inputText = document.querySelector("#inputlist")
+    let newItem = document.createElement('div')
+    newItem.classList.add('box')
+    newItem.innerHTML = `<input class="checkbox" type="checkbox">
+                        <span id="text">${inputText.value}</span>
+                        <button class="delete-btn">Delete</button>`
+
+    document.querySelector('.list').append(newItem)
+
+    deleteFunction()
+    checkboxFunction()
+}
+
+const deleteFunction = () => {
+    let deleteButtons = document.querySelectorAll(".delete-btn")
+
+    deleteButtons.forEach(deleteButton => {
+        deleteButton.onclick = () => {
+            deleteButton.parentNode.remove()
+        }
+    })
+}
+
+const checkboxFunction = () => {
+    let checkboxes = document.querySelectorAll(".checkbox")
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', event => {
+            checkbox.nextElementSibling.style.textDecoration = "none"
+
+            if (event.currentTarget.checked) {
+                checkbox.nextElementSibling.style.textDecoration = "line-through"
+            }
+        })
+    })
+}
+
+let form = document.querySelector("#add-new-todo")
+form.addEventListener("submit", addFunction, false)
